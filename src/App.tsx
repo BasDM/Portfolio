d629e4b01bc60a15f95a46e87ff1b71456e61233
BasDM <bas.demeurichy@gmail.com>
2025-04-26 23:06:17 +0200

    Add homescene

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
     <>
     <Navbar />
     <Router>
-      <div className='min-h-[calc(100vh-4rem)] p-8'>
+      <div className='min-h-[calc(100vh-4rem)]'>
       <Routes>
-          <Route path="/" element={<Home />} /> {/* Home route */}
-          <Route path="/projects" element={<Projects />} /> {/* Projects route */}
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </div>
     </Router>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,10 @@
+import Spline from '@splinetool/react-spline';
 
 function Home() {
     return (
-        <div className="">
-            <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
-            <p className="text-lg mb-8">Explore my projects and skills.</p>
-            <a href="/projects" className="bg-blue-500 text-white px-4 py-2 rounded-md">View Projects</a>
+        <div className="w-full h-[calc(100vh-4rem)]">
+            <Spline scene="https://prod.spline.design/3Evwac6TAsSRYVwC/scene.splinecode" />
         </div>
     );
 }
-
 export default Home;
