992546f87d406e4c3203297c7f0ed6fe660e6d02
BasDM <bas.demeurichy@gmail.com>
2025-04-27 21:05:14 +0200

    Update spline design

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,16 @@
 import Spline from '@splinetool/react-spline';
 
 function Home() {
+    function onMousePress(e:any) {
+        if (e.target.name === 'ProjectsFolder') {
+          alert('I have been clicked!');
+        }
+    }
     return (
         <div className="w-full h-[calc(100vh-4rem)]">
-            <Spline scene="https://prod.spline.design/3Evwac6TAsSRYVwC/scene.splinecode" />
+            <Spline scene="https://prod.spline.design/3Evwac6TAsSRYVwC/scene.splinecode" 
+            onSplineMouseDown={onMousePress}
+            />
         </div>
     );
 }
