353d18c3b8bbf78ed2a560bc2e1ed142e4f8bff4
BasDM <bas.demeurichy@gmail.com>
2025-11-24 11:21:09 +0100

    Add leading slash which was causing 404

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -27,7 +27,7 @@ function Navbar() {
 								Home
 							</a>
 							<a
-								href="aboutme"
+								href="/aboutme"
 								className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
 								About Me
 							</a>
